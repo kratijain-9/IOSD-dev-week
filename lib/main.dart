3fd71c84e128af936b8aa95a3b12451bb9eb3239
kratijain-9 <77618906+kratijain-9@users.noreply.github.com>
2021-01-21 19:47:21 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
     allPeople = [Person("Maanas", "Why so serious?")];
     allPeople.add(Person("Shubh", "Alright, Alright, Alright!"));
 //    allPeople.add(Person("Shubh", "Why so serious?"));
-allPeople.add(Person("Shivansh ", "hey there i am using github?"));
+    //    allPeople.add(Person("Shubh", "Why so serious?"));
 
     return MaterialApp(
       home: Scaffold(
